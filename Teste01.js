e806012c9d7f94b608803431f97d080f8c8fa5d8
Mauricio Carvalho <developer.mauricio1@outlook.com>
2020-02-20 15:34:22 -0300

    Primeiro Commit

---
+++ Teste01.js
@@ -0,0 +1,30 @@
+////$ git config --global user.name "Mauricio Carvalho"
+
+
+//git config--global user.email "developer.mauricio1@outlook.com"
+
+// Configurar o editor principal
+//git config --global core.editor code
+
+
+//Testei aqui:
+
+// git status
+
+// git commit -m "Qualquer coisa"
+
+// git add qualquercoisa.js
+
+//git log //Verificar os logs
+
+// git log --decorate
+
+//  git log --author="Mauricio Carvalho"
+
+// git shortlog // Ordem alfabetica dos autores
+
+//git shortlog -sn
+
+// git log --graph
+
+// git show ...
